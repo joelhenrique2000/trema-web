9ce8102a51bdccf130d55fbf301f92e013146c7b
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2021-07-06 19:15:38 -0300

    Criado o layout x-small para signin-section

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,7 +2,7 @@ export default () => {
   return (
     <div className="signin">
       <div className="signin-section">
-        <img src="" alt="" className="signin-section__logo" />
+        <img src="/trema.svg" alt="" className="signin-section__logo" />
         <div className="signin-section__container-form">
           <h1 className="signin-section__title">Bem-vindo de volta</h1>
           <div className="divider">
@@ -10,32 +10,28 @@ export default () => {
           </div>
           <form className="signin-form">
             <div className="form-group">
-              <label>Email</label>
+              <label className="form-label">Email</label>
               <input type="email" className="form-control" placeholder="Insira o email" id="email" />
             </div>
             <div className="form-group">
-              <label>Senha</label>
+              <label className="form-label">Senha</label>
               <input type="password" className="form-control" placeholder="Insira a email" id="password" />
             </div>
-            <div className="row">
-              <div className="col col-12 col-lg-6">
-                <div className="form-group">
-                  <div className="form-check">
-                    <input type="checkbox" className="form-check-input" id="keep-logged-in" />
-                    <label className="form-check-label" htmlFor="keep-logged-in">Keep me logged in</label>
-                  </div>
+            <div>
+              <div className="form-group">
+                <div className="form-check">
+                  <input type="checkbox" className="form-check__checkbox" id="keep-logged-in" checked />
+                  <label className="form-check__label" htmlFor="keep-logged-in">Keep me logged in</label>
                 </div>
               </div>
-              <div className="col col-12 col-lg-6 text-lg-right">
-                <div className="form-group">
-                  <a href="resetpw">Forgotten your password?</a>
-                </div>
+              <div className="form-group">
+                <a href="resetpw" className="link">Forgotten your password?</a>
               </div>
             </div>
-            <button type="submit" className="btn btn-block">Log in</button>
+            <button type="submit" className="btn btn--block">Log in</button>
           </form>
           <div className="divider"></div>
-            <p className="no-account">Não tem uma conta? <a href="">Crie uma aqui</a></p>
+            <p className="signin-section__without-account">Não tem uma conta? <a href="" className="link">Crie uma aqui</a></p>
           </div>
       </div>
       <div className="signin-hero">
