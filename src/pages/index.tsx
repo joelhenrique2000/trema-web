d606c88d35e597b871f731af5566f3e61b6e8f70
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2021-07-06 22:24:10 -0300

    Criado as págians

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,28 @@
 export default () => {
   return (
-    <div>hello world</div>
+    <div className="home">
+      <div className="home__container">
+        <div className="sidebar">
+          
+        </div>
+        <div className="board-group">
+            <div className="board-group__title">Quadros com Estrelas</div>
+            <div className="board-group__container">
+              <div className="board">
+                <div className="board__header">
+                  <span className="board__header-title">Kaban</span>
+                </div>
+                <div className="board-footer">
+                  <span className="board-footer__title">Lorem</span>
+                  <div className="board-footer__container-icon">
+                    <span>V</span>
+                    <span>S</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
   )
 }
